d98c497841fbe44bb661b0cc49b07c6aeb093a3c
Marcos Brandan <marcosbrandan681@gmail.com>
2024-11-01 19:45:12 -0300

    se agregaron las clases Gerente, Candidato y Usuario

---
+++ TFIDS/src/tfids/Gerente.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tfids;
+
+import java.util.Date;
+
+/**
+ *
+ * @author marco
+ */
+public class Gerente {
+    
+    private int dni;
+    private String nombreApellido;
+    private Date fechaNacimiento;
+    private int celular;
+    private String email;
+    
+    public Gerente(){
+        
+    }
+    
+}
---
+++ TFIDS/src/tfids/Usuario.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tfids;
+
+/**
+ *
+ * @author marco
+ */
+public class Usuario {
+    
+    private int id_usuario;
+    private String username;
+    private String password;
+    private Gerente gerente;
+    
+    
+    
+}
